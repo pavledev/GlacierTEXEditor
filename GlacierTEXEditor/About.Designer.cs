4e8423cebbc93a1b0e8a9013ed4d0efe87263994
Pavle <pavle_nis@yahoo.com>
2020-07-05 00:55:07 +0200

    Update About.Designer.cs

--- GlacierTEXEditor/About.Designer.cs
+++ GlacierTEXEditor/About.Designer.cs
@@ -73,7 +73,7 @@
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.Location = new System.Drawing.Point(20, 134);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(529, 140);
+            this.label4.Size = new System.Drawing.Size(587, 140);
             this.label4.TabIndex = 3;
             this.label4.Text = resources.GetString("label4.Text");
             // 
